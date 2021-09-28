e4570c617f34f4afbc31a49ab14e3b54f72d9c43
cpneppal <cpneppal@buffalo.edu>
2021-09-28 16:35:00 -0400

    changed main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import bottle
+import os
 
+
+file_name_dict = {
+    "bootstrap.min.css": "assets/bootstrap/css/bootstrap.min.css",
+    "bootstrap.min.js": "assets/bootstrap/js/bootstrap.min.js",
+    "styles.css": "assets/css/Login-Form-Dark.css",
+
+}
 
 @bottle.route('/')
 def rethtml():
     return bottle.static_file("index.html", ".")
+
+
+
+@bottle.route('/<filename>')
+def route_dependencies(filename):
+    return bottle.static_file(filename, ".")
+
 
 
 
