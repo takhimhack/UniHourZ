208e455f5bd8e426ffeaf911a8f3de593a7b7c16
cpneppal <cpneppal@buffalo.edu>
2021-09-28 16:44:20 -0400

    changed main.py

--- main.py
+++ main.py
@@ -15,13 +15,24 @@ def rethtml():
 
 
 
-@bottle.route('/<filename>')
-def route_dependencies(filename):
-    return bottle.static_file(filename, ".")
+@bottle.route('/assets/bootstrap/css/<filename>')
+def return_bootstrap_css(filename):
+    return bottle.static_file(filename, "./assets/bootstrap/css/")
 
+@bottle.route('/assets/bootstrap/js/<filename>')
+def return_bootstrap_js(filename):
+    return bottle.static_file(filename, "./assets/bootstrap/js/")
 
+@bottle.route('/assets/css/<filename>')
+def return_css(filename):
+    return bottle.static_file(filename, "./assets/css/")
 
+@bottle.route('/assets/fonts/<filename>')
+def return_fonts(filename):
+    return bottle.static_file(filename, "./assets/fonts/")
 
-
+@bottle.route('/assets/img/<filename>')
+def return_fonts(filename):
+    return bottle.static_file(filename, "./assets/img/")
 
 bottle.run(host="0.0.0.0", port=8000)
