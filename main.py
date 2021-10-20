66c3928e25ae44d4f6a44f8393f8c51bc95b6622
takhimhack <takhimhaque2@gmail.com>
2021-10-20 12:45:48 -0400

    Attempted to implement auth for current user  #28
    The feature is not done yet. For some reason I can't get retrieve the profile information of each user.

--- main.py
+++ main.py
@@ -17,36 +17,17 @@ if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
 
     @bottle.route('/')
-    def rethtml():
+    def ret_html():
         return bottle.static_file("index.html", ".")
 
     @bottle.route('/<filename>')
-    def rethtml(filename):
-        return bottle.static_file(filename, ".")
+    def ret_html_2(filename):
+        return bottle.static_file(filename, ".") if ".html" in filename else None
 
-    @bottle.route('/assets/bootstrap/css/<filename>')
-    def return_bootstrap_css(filename):
-        return bottle.static_file(filename, "./assets/bootstrap/css/")
-
-    @bottle.route('/assets/bootstrap/js/<filename>')
-    def return_bootstrap_js(filename):
-        return bottle.static_file(filename, "./assets/bootstrap/js/")
-
-    @bottle.route('/assets/css/<filename>')
-    def return_css(filename):
-        return bottle.static_file(filename, "./assets/css/")
-
-    @bottle.route('/assets/frontendjs/<filename>')
-    def return_client_js(filename):
-        return bottle.static_file(filename, "./assets/frontendjs/")
-
-    @bottle.route('/assets/fonts/<filename>')
-    def return_fonts(filename):
-        return bottle.static_file(filename, "./assets/fonts/")
-
-    @bottle.route('/assets/img/<filename>')
-    def return_img(filename):
-        return bottle.static_file(filename, "./assets/img/")
+    @bottle.route('/assets/<filename:path>')
+    def ret_assets(filename):
+        return bottle.static_file(filename, "./assets")
+        
 
     @bottle.post('/userRegistration')
     def validate_registration():
