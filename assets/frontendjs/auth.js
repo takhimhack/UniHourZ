72911e187a276dea3d268c7a9f5463b6bcb43cfb
cwilson22 <csw8@buffalo.edu>
2021-10-19 20:28:09 -0400

    Converted to javascript authentication #32
    Changed over to auth.js for authentication through firebase. Failed login functionality still converted as well.

---
+++ assets/frontendjs/auth.js
@@ -0,0 +1,100 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', event => {
+  event.preventDefault()
+})
+
+function submit_registration() {
+  let json_obj = JSON.stringify({
+    name: retrieve_name(),
+    email: retrieve_email(),
+    password: retrieve_password(),
+    typeofUser: retrieve_registration_select(),
+  });
+  console.log(json_obj);
+  ajaxPostRequest("/userRegistration", json_obj, process_auth_response);
+}
+
+function submit_login() {
+  let json_obj = JSON.stringify({
+    email: retrieve_email(),
+    password: retrieve_password(),
+  });
+  console.log(json_obj);
+  ajaxPostRequest("/userLogin", json_obj, process_auth);
+}
+
+function retrieve_name() {
+  return document.getElementById("Name") !== null
+    ? document.getElementById("Name").value
+    : "";
+}
+
+function retrieve_email() {
+  return document.getElementById("inputemail") !== null
+    ? document.getElementById("inputemail").value
+    : "";
+}
+
+function retrieve_password() {
+  if (document.getElementById("password") !== null) {
+    return document.getElementById("password").value;
+  } else if (document.getElementById("password_two") !== null) {
+    return document.getElementById("password_two").value;
+  } else {
+    return "";
+  }
+}
+
+function retrieve_registration_select() {
+  if (document.getElementById("reg_select") !== null) {
+    loginType = "registration";
+    let selector = document.getElementById("reg_select");
+    return selector.options[selector.selectedIndex].value === "12"
+      ? "Student"
+      : "Instructor";
+  } else {
+    return "Student";
+  }
+}
+
+function process_auth_response(login_response) {
+  let decoded_response = JSON.parse(login_response);
+  /* We have an invalid login from the web server */
+  if (decoded_response.valid !== "valid") {
+    document.getElementById("errormsg").innerHTML = decoded_response.valid;
+  } else {
+    document.getElementById("errormsg").innerHTML = "";
+  }
+}
+
+function process_auth(login_response) {
+  let decoded_response = JSON.parse(login_response);
+  if (decoded_response.valid !== "valid") {
+    document.getElementById("errorf").innerHTML = decoded_response.message;
+  } else {
+    window.location="home.html";
+    document.getElementById("welcome").innerHTML = decoded_response.user;
+  }
+}
+
+function ajaxGetRequest(path, callback) {
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      callback(this.response);
+    }
+  };
+  request.open("GET", path);
+  request.send();
+}
+
+function ajaxPostRequest(path, data, callback) {
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      callback(this.response);
+    }
+  };
+  request.open("POST", path);
+  request.send(data);
+}
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
 import bottle
+import json
 import os
 import requests
-import fireapp
+
+import server_code.FirebaseAPI.firebaseAPI as fire
+from server_code.parse_login.parse_login import parse_email
+from server_code import client_validator
+from server_code.FirebaseAPI.Registration import registerUser
 
 
 if __name__ == "__main__":
@@ -10,26 +15,11 @@ if __name__ == "__main__":
 
     @bottle.route('/')
     def rethtml():
-        return bottle.template("index.html", errormessage="")
+        return bottle.static_file("index.html", ".")
 
-    # @bottle.route('/<filename>')
-    # def rethtml(filename):
-    #     return bottle.static_file(filename, ".")
-
-    @bottle.post('/')
-    def do_login():
-        password = bottle.request.forms.get("password")
-        email = bottle.request.forms.get("email")
-        try:
-            user = fireapp.auth.sign_in_with_email_and_password(email, password)
-            return bottle.template("home.html", username=email)
-        except requests.HTTPError as err:
-            if err.strerror[err.strerror.find("message")+11:err.strerror.find("message")+26] == 'EMAIL_NOT_FOUND':
-                return bottle.template("index.html", errormessage="Invalid email")
-            elif err.strerror[err.strerror.find("message")+11:err.strerror.find("message")+27] == 'INVALID_PASSWORD':
-                return bottle.template("index.html", errormessage="Invalid password")
-            else:
-                return bottle.template("index.html", errormessage="Error logging in")
+    @bottle.route('/<filename>')
+    def rethtml(filename):
+        return bottle.static_file(filename, ".")
 
     @bottle.route('/assets/bootstrap/css/<filename>')
     def return_bootstrap_css(filename):
@@ -43,6 +33,10 @@ if __name__ == "__main__":
     def return_css(filename):
         return bottle.static_file(filename, "./assets/css/")
 
+    @bottle.route('/assets/frontendjs/<filename>')
+    def return_client_js(filename):
+        return bottle.static_file(filename, "./assets/frontendjs/")
+
     @bottle.route('/assets/fonts/<filename>')
     def return_fonts(filename):
         return bottle.static_file(filename, "./assets/fonts/")
@@ -50,5 +44,47 @@ if __name__ == "__main__":
     @bottle.route('/assets/img/<filename>')
     def return_img(filename):
         return bottle.static_file(filename, "./assets/img/")
+
+    @bottle.post('/userRegistration')
+    def validate_registration():
+        response = bottle.request.body.read().decode()
+        decoded_response = client_validator.sanitize_input(response)
+        # check if registering
+        if client_validator.contains(decoded_response, ['email', 'name', 'password', 'typeofUser']):
+            valid_state = parse_email(decoded_response['email'], 'buffalo.edu')
+            if valid_state != 'valid':
+                return json.dumps({
+                    'valid': valid_state
+                })
+            valid_state = registerUser(decoded_response)
+            return json.dumps({
+                'valid': valid_state
+            })
+        else:
+            return json.dumps({
+                'valid': 'invalid!'
+            })
+
+    @bottle.post('/userLogin')
+    def validate_login():
+        response = bottle.request.body.read().decode()
+        decoded_response = client_validator.sanitize_input(response)
+        if client_validator.contains(decoded_response, ['email', 'password']):
+            valid_state = parse_email(decoded_response['email'], 'buffalo.edu')
+            if valid_state != 'valid':
+                return json.dumps({'valid': 'invalid!', 'message': "Enter valid email address"})
+            try:
+                user = fire.auth.sign_in_with_email_and_password(decoded_response["email"], decoded_response["password"])
+                return json.dumps({'valid': 'valid', 'user': decoded_response["email"]})
+            except requests.HTTPError as err:
+                if err.strerror[err.strerror.find("message") + 11:err.strerror.find("message") + 26] == 'EMAIL_NOT_FOUND':
+                    return json.dumps({'valid': 'invalid!', 'message': "No account with that email"})
+                elif err.strerror[
+                     err.strerror.find("message") + 11:err.strerror.find("message") + 27] == 'INVALID_PASSWORD':
+                    return json.dumps({'valid': 'invalid!', 'message': "Invalid password"})
+                else:
+                    return json.dumps({'valid': 'invalid!', 'message': "Login error"})
+        else:
+            return json.dumps({'valid': 'invalid!', 'message': "Enter email and password"})
 
     bottle.run(host="0.0.0.0", port=port)
