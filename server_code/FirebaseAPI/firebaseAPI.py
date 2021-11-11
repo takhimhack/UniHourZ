b566188e7643ac8bf3ab4c35c64cd1fcb06bc857
takhimhack <takhimhaque2@gmail.com>
2021-11-10 23:48:47 -0500

    Fixed the services.json #68
    Remove firebase config keys for heroku

--- server_code/FirebaseAPI/firebaseAPI.py
+++ server_code/FirebaseAPI/firebaseAPI.py
@@ -1,5 +1,4 @@
 import os
-import json
 from firebase import Firebase
 
 '''
@@ -26,25 +25,8 @@ config_server = {
    "storageBucket": os.environ.get('storageBucket'),
    "serviceAccount": "service_account/service.json"
 }
-server_keys = {
-  "type": os.environ.get("type"),
-  "project_id": os.environ.get("project_id"),
-  "private_key_id": os.environ.get("private_key_id"),
-  "private_key": os.environ.get("private_key"),
-  "client_email": os.environ.get("client_email"),
-  "client_id": os.environ.get("client_id"),
-  "auth_uri": os.environ.get("auth_uri"),
-  "token_uri": os.environ.get("token_uri"),
-  "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
-  "client_x509_cert_url": os.environ.get("client_x509_cert_url")
-}
 
-if not os.path.isdir("service_account"):
-  os.mkdir("service_account")
 
-if not os.path.isfile("service_account/service.json"):
-  with open("service_account/service.json", "w") as file:
-    json.dump(server_keys, file)
 
 # firebase = Firebase(config_user)
 firebase_server = Firebase(config_server)
